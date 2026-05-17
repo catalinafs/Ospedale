68df07fd4f32cb61eeee7f14a5cca091208455f3
Javier Leguízamo <jaleguizamo@uninorte.edu.co>
2026-05-17 12:57:00 -0500

    feat: AppointmentController

---
+++ src/core/controllers/AppointmentController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.controllers;
+
+/**
+ *
+ * @author briggoes
+ */
+public class AppointmentController {
+
+    static boolean requestAppointment(boolean type, String date, String time,
+            long docId, long patientId, String reason) { // TODO: implement
+        return false;
+    }
+}
