4b8512468d46f1980bad387f7555f602234afe7e
Javier Leguízamo <jaleguizamo@uninorte.edu.co>
2026-05-17 12:57:38 -0500

    feat: PatientController

---
+++ src/core/controllers/PatientController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.controllers;
+
+import core.model.Storage;
+
+/**
+ *
+ * @author briggoes
+ */
+public class PatientController {
+
+    static private Storage storage = Storage.getInstance();
+
+    static boolean registerPatient(long id, String username, String fname,
+            String lname, String password, String email, String birthdate,
+            boolean gender, long phone, String address) { // TODO: implement
+
+        if (fname.trim().equals("") || lname.trim().equals("") || username.trim().equals("")) {
+            return false;
+        }
+
+        return false;
+    }
+
+    static boolean updatePatientData(long id, String username, String fname,
+            String lname, String password, String email, String birthdate,
+            boolean gender, long phone, String address) { // TODO: implement
+        return false;
+    }
+
+}
