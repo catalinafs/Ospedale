48711a8bf3f4d2b872ea320288ac8733b36dd1c0
Javier Leguízamo <jaleguizamo@uninorte.edu.co>
2026-05-17 12:57:52 -0500

    feat: getUser(username)

--- src/core/model/Storage.java
+++ src/core/model/Storage.java
@@ -11,22 +11,35 @@ import java.util.HashMap;
  * @author daniel
  */
 public class Storage {
+
     private static Storage instance = null;
     private static HashMap<Long, Object> storage;
-    
-    private Storage(){
-        storage = new HashMap<Long,Object>();
+
+    private Storage() {
+        storage = new HashMap<Long, Object>();
     }
-    
-    public static Storage getInstance(){
-        if(instance == null){
+
+    public static Storage getInstance() {
+        if (instance == null) {
             instance = new Storage();
             return instance;
         }
         return instance;
     }
+
+    public User getUser(Long id) {
+        return (User) storage.get(id);
+    }
     
-    public User getUser(Long id){
-        return (User)storage.get(id);
+    public User getUser(String username) {
+        for (Object value : storage.values()) {
+            if (value instanceof User) {
+                User user = (User) value;
+                if (user.getUsername().equals(username)) {
+                    return user;
+                }
+            }
+        }
+        return null;
     }
 }
