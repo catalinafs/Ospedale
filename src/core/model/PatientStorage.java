035cbc802ebd2f4028a6e38dbbfa3bf59edb7609
Javier Leguízamo <jaleguizamo@uninorte.edu.co>
2026-05-24 02:55:11 -0500

    feat: implement getPattient(fullname) and getAllPattients()

--- src/core/model/PatientStorage.java
+++ src/core/model/PatientStorage.java
@@ -11,21 +11,22 @@ import java.util.ArrayList;
  *
  * @author daniel
  */
-public class PatientStorage implements IPatientStorage{
+public class PatientStorage implements IPatientStorage {
+
     private static PatientStorage instance = null;
     private final IUserStorage userStorage;
-    
+
     private PatientStorage(IUserStorage userStorage) {
         this.userStorage = userStorage;
     }
-    
+
     public static PatientStorage getInstance(IUserStorage userStorage) {
         if (instance == null) {
             instance = new PatientStorage(userStorage);
         }
         return instance;
     }
-    
+
     @Override
     public Patient getPatient(long id) {
         User user = userStorage.get(id);
@@ -34,7 +35,7 @@ public class PatientStorage implements IPatientStorage{
         }
         return null;
     }
-    
+
     @Override
     public Patient getPatientByUsername(String username) {
         User user = userStorage.get(username);
@@ -43,12 +44,12 @@ public class PatientStorage implements IPatientStorage{
         }
         return null;
     }
-    
+
     @Override
     public boolean addPatient(Patient patient) {
         return userStorage.add(patient);
     }
-    
+
     @Override
     public boolean updatePatient(long id, String username, String firstname,
             String lastname, String password, String email, LocalDate birthdate,
@@ -90,7 +91,7 @@ public class PatientStorage implements IPatientStorage{
     private static boolean hasText(String value) {
         return value != null && !value.trim().isEmpty();
     }
-    
+
     @Override
     public ArrayList<Patient> getAllPatients() {
         ArrayList<Patient> list = new ArrayList<>();
@@ -100,5 +101,15 @@ public class PatientStorage implements IPatientStorage{
             }
         }
         return list;
+    }
+
+    @Override
+    public Patient getPatientByFullName(String fullname) {
+        var names = fullname.split(" ");
+        User user = userStorage.get(names[0], names[1]);
+        if (user instanceof Patient pat) {
+            return pat;
+        }
+        return null;
     }
 }
